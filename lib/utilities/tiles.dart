875d926bcc94b36aa662c4af301189b39085c917
Abhinav Rajeev Kumar <abku2504@gmail.com>
2023-03-25 03:03:36 +0530

    Added curvature to the boxes and shade of grey

--- lib/utilities/tiles.dart
+++ lib/utilities/tiles.dart
@@ -10,6 +10,12 @@ class SquareTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.grey[100]
+      ),
       child: Image.asset(
         imagePath,
         height: 40,
